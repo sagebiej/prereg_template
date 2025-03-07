62e4e2241d82b81dbb51c02644114dadd58c3472
Julian Sagebiel <julian.sagebiel@idiv.de>
2025-03-07 15:06:58 +0100

    added script to render different versions

---
+++ run_render.R
@@ -0,0 +1,4 @@
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "answers" , output_file = "Preregistration_Template_with_answers.html" , output_format = "html")
+
+
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , output_file = "Preregistration_Template.html" , output_format = "html")
