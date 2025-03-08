635006e02ca845ece18adc00c3e6fd95f4edbf6d
Julian Sagebiel <julian.sagebiel@idiv.de>
2025-03-08 20:09:27 +0100

    added automated rendering in different formats and small improvements in template

--- run_render.R
+++ run_render.R
@@ -1,4 +1,13 @@
 quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "answers" , output_file = "Preregistration_Template_with_answers.html" , output_format = "html")
 
-
 quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , output_file = "Preregistration_Template.html" , output_format = "html")
+
+
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "answers" , output_file = "Preregistration_Template_with_answers.pdf" , output_format = "pdf")
+
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , output_file = "Preregistration_Template.pdf" , output_format = "pdf")
+
+
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , output_file = "Preregistration_Template.docx" , output_format = "docx")
+
+quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "answers"  , output_file = "Preregistration_Template_with_answers.docx" , output_format = "docx")
