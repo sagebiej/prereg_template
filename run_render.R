7076b0c3e6a166c8490776ce42e5c9a67a285ae4
Julian Sagebiel <julian.sagebiel@idiv.de>
2025-04-02 10:36:01 +0200

    added tex files as templates

--- run_render.R
+++ run_render.R
@@ -4,6 +4,7 @@ quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , out
 
 
 quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "answers" , output_file = "Preregistration_Template_with_answers.pdf" , output_format = "pdf")
+file.rename("Preregistration_Template.tex","Preregistration_Template_with_answers.tex")
 
 quarto::quarto_render(input = "Preregistration_Template.qmd", profile = "" , output_file = "Preregistration_Template.pdf" , output_format = "pdf")
 
